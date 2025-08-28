62e7a9437b1ee924c9a9094d0c8537d0d791ac8c
Tushar <chauhantushar6706@gmail.com>
2025-08-28 20:25:33 +0530

    more patterns

--- patterns/12.cpp
+++ patterns/12.cpp
@@ -5,12 +5,13 @@ int main()
 {
     int n = 5;
     int row = 1; 
-    char count = 'A';
     while(row<=n){
-
+        
         for(int col =1 ; col<=n;col++){
+            
+            char count = 'A' + row+col -2 ;
             cout<<count;
-            count++;
+            
         }
         row++;
         cout<<endl;
