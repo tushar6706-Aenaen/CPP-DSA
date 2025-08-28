d5f49fb9058c9f2be53fa71cc27415cca07b2e81
Tushar <chauhantushar6706@gmail.com>
2025-08-28 22:12:52 +0530

    no.17 patterns

---
+++ patterns/17.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int n  = 4 ; 
+    int row = 1;
+
+    while(row <=n){
+        int col= n-row+1 ;        
+        while(col){
+            col--;
+            cout<<"*";
+        }
+        row++;
+        cout<<endl;
+    }
+    return 0;
+}
